7b10700808bb789e19a64509b3e7c1d7ccf94ec6
Joshua Uhalt, Ph.D. <Josh.Uhalt@gmail.com>
2026-08-18 10:08:54 -0400

    Add manuscript publication stages

---
+++ ManuscriptPipeline/Models/PaperStage.vb
@@ -0,0 +1,14 @@
+﻿Namespace Models
+
+    Public Enum PaperStage
+        Idea
+        Draft
+        Submitted
+        UnderReview
+        Revision
+        Accepted
+        InPress
+        Published
+    End Enum
+
+End Namespace
